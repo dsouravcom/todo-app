fbee127ac3529c83dafdbc7ed9c445505a8e58c0
Sourav dutta <souravduttasd2@gmail.com>
2024-04-15 18:07:00 +0530

    bug fix

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const Task = require("./models/Task.js");
+const Task = require("./models/task.js");
 require("dotenv").config();
 
 const app = express();
